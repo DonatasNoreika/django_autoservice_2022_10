10045c67b814699020f95e2a08413b27b4c96bea
Donatas Noreika <donoras@gmail.com>
2022-10-03 08:58:01 +0300

    patobulinti modeliai

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -6,11 +6,15 @@ class AutomobilioModelis(models.Model):
     gamintojas = models.CharField("Gamintojas", max_length=100)
     modelis = models.CharField("Modelis", max_length=100)
 
+    def __str__(self):
+        return f"{self.gamintojas} {self.modelis}"
 
 class Paslauga(models.Model):
     pavadinimas = models.CharField("Pavadinimas", max_length=200)
     kaina = models.FloatField("Kaina")
 
+    def __str__(self):
+        return f"{self.pavadinimas}"
 
 class Automobilis(models.Model):
     modelis = models.ForeignKey("AutomobilioModelis", on_delete=models.SET_NULL, null=True)
@@ -18,13 +22,21 @@ class Automobilis(models.Model):
     vin_kodas = models.CharField("VIN kodas", max_length=20)
     kliento_vardas = models.CharField("Kliento vardas", max_length=30)
 
+    def __str__(self):
+        return f"{self.modelis} {self.valstybinis_nr} ({self.kliento_vardas})"
 
 class Uzsakymas(models.Model):
     data = models.DateField("Data", auto_now_add=True)
     automobilis = models.ForeignKey("Automobilis", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.data} {self.automobilis}"
 
 
 class UzsakymoEilute(models.Model):
     uzsakymas = models.ForeignKey("Uzsakymas", on_delete=models.CASCADE, null=True)
     paslauga = models.ForeignKey("Paslauga", on_delete=models.SET_NULL, null=True)
     kiekis = models.IntegerField("Kiekis")
+
+    def __str__(self):
+        return f"{self.uzsakymas} {self.paslauga} {self.kiekis}"
