72d4d6dce7fb95b6bca8c970a805a00700bace60
Donatas Noreika <donoras@gmail.com>
2022-10-03 08:45:56 +0300

    modeliai pridėti į admin

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (AutomobilioModelis,
+                     Automobilis,
+                     Paslauga,
+                     Uzsakymas,
+                     UzsakymoEilute)
 
 # Register your models here.
+admin.site.register(AutomobilioModelis)
+admin.site.register(Automobilis)
+admin.site.register(Paslauga)
+admin.site.register(Uzsakymas)
+admin.site.register(UzsakymoEilute)
