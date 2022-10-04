346a502405ae400f551bd312680ac508a686e1e3
Donatas Noreika <donoras@gmail.com>
2022-10-04 08:14:47 +0300

    3 paskaita, 2, 3, 4, 5 užduotis done

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -5,9 +5,28 @@ from .models import (AutomobilioModelis,
                      Uzsakymas,
                      UzsakymoEilute)
 
+
+class UzsakymoEiluteInline(admin.TabularInline):
+    model = UzsakymoEilute
+    extra = 0
+
+
+class UzsakymasAdmin(admin.ModelAdmin):
+    list_display = ('automobilis', 'data')
+    inlines = [UzsakymoEiluteInline]
+
+
+class AutomobilisAdmin(admin.ModelAdmin):
+    list_display = ('modelis', 'valstybinis_nr', 'vin_kodas', 'kliento_vardas')
+
+
+class PaslaugaAdmin(admin.ModelAdmin):
+    list_display = ('pavadinimas', 'kaina')
+
+
 # Register your models here.
 admin.site.register(AutomobilioModelis)
-admin.site.register(Automobilis)
-admin.site.register(Paslauga)
-admin.site.register(Uzsakymas)
+admin.site.register(Automobilis, AutomobilisAdmin)
+admin.site.register(Paslauga, PaslaugaAdmin)
+admin.site.register(Uzsakymas, UzsakymasAdmin)
 admin.site.register(UzsakymoEilute)
