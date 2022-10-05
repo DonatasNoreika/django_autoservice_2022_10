38afd0a3fa94abba5593e69fa40e6539ca31ea10
Donatas Noreika <donoras@gmail.com>
2022-10-05 08:47:36 +0300

    4 paskaita: 4 užduotis done

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Paslauga, Uzsakymas, Automobilis
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    kontekstas = {
+        'paslaugu_kiekis': Paslauga.objects.all().count(),
+        'atliktu_uzsakymu_kiekis': Uzsakymas.objects.filter(statusas__exact='i').count(),
+        'automobiliu_kiekis': Automobilis.objects.all().count(),
+    }
+    return render(request, 'index.html', context=kontekstas)
